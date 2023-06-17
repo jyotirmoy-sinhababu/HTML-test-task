f987715919c709e4cf4e070241c0776c5992c66e
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-17 13:03:29 +0530

    style added to nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 
+import HomePage from './page/home/HomePage';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
