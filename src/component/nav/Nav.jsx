20b39dac0c086a8c3cdb3e534385912cee5b2b1f
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-17 12:54:46 +0530

    style added

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+import './navStyle.css';
+
 import navImg from '../../asset/img/navImg.png';
 
 const Nav = () => {
   return (
-    <div>
-      <div>
-        <img src={navImg} alt='' />
+    <div className='nav-main-cnt'>
+      <div className='nav-img-cnt'>
+        <img className='nav-img' src={navImg} alt='nav-head-img' />
       </div>
-      <div></div>
+      <div className='nav-btn-cnt'>
+        <button className='nav-btn'>Nav 1</button>
+        <button className='nav-btn'>Nav 2</button>
+        <button className='nav-btn'>Nav 3</button>
+        <button className='nav-btn'>Nav 4</button>
+        <button className='nav-btn'>Nav 5</button>
+        <button className='nav-btn'>Nav 6</button>
+        <button className='nav-btn'>Nav 7</button>
+        <button className='nav-btn'>Nav 8</button>
+      </div>
     </div>
   );
 };
