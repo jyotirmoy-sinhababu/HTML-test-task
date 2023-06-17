95d0715a6cf8d87e24eecc740d6a802758095a0f
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-17 12:47:10 +0530

    react app create, nav bar created

---
+++ src/component/nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import navImg from '../../asset/img/navImg.png';
+
+const Nav = () => {
+  return (
+    <div>
+      <div>
+        <img src={navImg} alt='' />
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Nav;
