0ea27dbe673b9aea1fcc6d8d737c9a311dbeb831
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-18 16:50:27 +0530

    bootstrap added to card

--- src/component/firstCardCnt/FirstCardCnt.jsx
+++ src/component/firstCardCnt/FirstCardCnt.jsx
@@ -1,15 +1,84 @@
 import React from 'react';
-import Card from '../cards/Card';
 
-import './firstCardStyle.css';
+import Card from 'react-bootstrap/card';
 
 const FirstCardCnt = () => {
   return (
-    <div className='main-firstCard-cnt'>
-      <div className='firstCard-cnt'>
-        <Card />
-        <Card />
-        <Card />
+    <div
+      style={{
+        display: 'flex',
+        padding: '101px',
+        justifyContent: 'space-around',
+      }}
+    >
+      <div>
+        <Card
+          style={{
+            height: '300px',
+            display: 'flex',
+            paddingTop: '45px',
+            gap: '30px',
+            width: '300px',
+            padding: '10px',
+            boxShadow: '0px 3px 6px #00000029',
+          }}
+        >
+          <Card.Title style={{ display: 'flex', justifyContent: 'center' }}>
+            Lorem ipsum dolor sit .
+          </Card.Title>
+          <Card.Text style={{ display: 'flex', justifyContent: 'center' }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
+            commodo est.Pellentesque eu placerat mauris. Etiam ut maximus ante.
+            Suspendisse varius dolor nec bibendum dignissim. Proin in diam a
+            justo sollicitudin mattis eu sagittisnunc. In hac habitasse.
+          </Card.Text>
+        </Card>
+      </div>
+      <div>
+        <Card
+          style={{
+            height: '300px',
+            display: 'flex',
+            paddingTop: '45px',
+            gap: '30px',
+            width: '300px',
+            padding: '10px',
+            boxShadow: '0px 3px 6px #00000029',
+          }}
+        >
+          <Card.Title style={{ display: 'flex', justifyContent: 'center' }}>
+            Lorem ipsum dolor sit .
+          </Card.Title>
+          <Card.Text style={{ display: 'flex', justifyContent: 'center' }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
+            commodo est.Pellentesque eu placerat mauris. Etiam ut maximus ante.
+            Suspendisse varius dolor nec bibendum dignissim. Proin in diam a
+            justo sollicitudin mattis eu sagittisnunc. In hac habitasse.
+          </Card.Text>
+        </Card>
+      </div>
+      <div>
+        <Card
+          style={{
+            height: '300px',
+            display: 'flex',
+            paddingTop: '45px',
+            gap: '30px',
+            width: '300px',
+            padding: '10px',
+            boxShadow: '0px 3px 6px #00000029',
+          }}
+        >
+          <Card.Title style={{ display: 'flex', justifyContent: 'center' }}>
+            Lorem ipsum dolor sit .
+          </Card.Title>
+          <Card.Text style={{ display: 'flex', justifyContent: 'center' }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
+            commodo est.Pellentesque eu placerat mauris. Etiam ut maximus ante.
+            Suspendisse varius dolor nec bibendum dignissim. Proin in diam a
+            justo sollicitudin mattis eu sagittisnunc. In hac habitasse.
+          </Card.Text>
+        </Card>
       </div>
     </div>
   );
